544018228a7fe98160ad9a7fd8801eadcd0573a5
Linqi Liu <liulinq@shernet.sheridancollege.ca>
2026-03-16 11:03:13 -0400

    Completed Turn Based Battler 2

--- Assets/Scripts/GrowCoroutine.cs
+++ Assets/Scripts/GrowCoroutine.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrowCoroutine : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Transform thing;
     public AnimationCurve curve;
+    public Button button;
     void Start()
     {
         thing.localScale = Vector2.zero;
@@ -19,8 +21,14 @@ public class GrowCoroutine : MonoBehaviour
         
     }
 
-    IEnumerator Grow()
+    public void growFlora()
     {
+        StartCoroutine(Grow());
+    }
+
+    public IEnumerator Grow()
+    {
+        button.interactable = false;
         float t = 0;
         thing.localScale = Vector2.zero;
         while (t < 1)
@@ -29,6 +37,7 @@ public class GrowCoroutine : MonoBehaviour
             thing.localScale = Vector2.one * curve.Evaluate(t);
             yield return null;
         }
+        button.interactable = true;
 
     }
 }
