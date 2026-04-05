e56e6167102456a9bf02d03bb56c74bbc4575217
thunderfern <115232330+Stwikey@users.noreply.github.com>
2026-04-05 15:08:06 -0400

    Fish now flips when spawning on the right

--- Assets/Scripts/SystemTask/FishSpawner.cs
+++ Assets/Scripts/SystemTask/FishSpawner.cs
@@ -8,7 +8,7 @@ public class FishSpawner : MonoBehaviour
 {
     public List<GameObject> fish;
     public GameObject player;
-    public float playerSize;
+    public Vector3 playerSize;
     public float fishSize;
     public Vector2 fishLocation;
     int fishSide;
@@ -24,13 +24,15 @@ public class FishSpawner : MonoBehaviour
     void Update()
     {
         //getting the current player size
-        playerSize = player.transform.localScale.x;
+        playerSize = player.transform.localScale;
 
     }
 
     //spawning fish
     void spawnFish()
     {
+        //getting the player size without changing it
+        scale = new Vector3(player.transform.localScale.z, player.transform.localScale.z, player.transform.localScale.z);
         fishSide = Random.Range(0, 2);
         if(fishSide == 0)
         {
@@ -40,9 +42,12 @@ public class FishSpawner : MonoBehaviour
         else
         {
             //pick a random location on the right
-            fishLocation = new Vector2(-10f, Random.Range(-5, 5));
+            fishLocation = new Vector2(10f, Random.Range(-5, 5));
+            scale.x = scale.x * -1;
+
         }
         GameObject spawnedFish = Instantiate(fish[0], fishLocation, Quaternion.identity);
+        spawnedFish.transform.localScale = scale;
     }
 
     /**public IEnumerator Spawn()
